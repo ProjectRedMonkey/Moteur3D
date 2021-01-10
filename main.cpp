7b13091890aa2cf85b74b412a378a531f913124d
ProjectRedMonkey <monkeydzoror@gmail.com>
2021-01-10 15:50:34 +0100

    Function triangle modified to fill it

--- main.cpp
+++ main.cpp
@@ -11,31 +11,29 @@ const TGAColor blue   = TGAColor(0, 0,   255,   255);
 Model *model = NULL;
 
 /**
- * Draw a line between two point
- * @param x0 abscissa of first point
- * @param y0 ordinate of the first point
- * @param x1 abscissa of second point
- * @param y1 ordinate of the second point
- * @param image to print the line
+ *  * Draw a line between two point
+ * @param p0 first vertice
+ * @param p1 second vertice
+ * @param image to use
  * @param color choosen
  */
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
-    if (x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if (p0.x > p1.x) {
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
     }
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
     int derror2 = std::abs(dy) * 2;
     int error2 = 0;
-    int y = y0;
-    for (int x = x0; x <= x1; x++) {
+    int y = p0.y;
+    for (int x = p0.x; x <= p1.x; x++) {
         if (steep) {
             image.set(y, x, color);
         } else {
@@ -50,12 +48,58 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 /**
- * Draw a triangle
+ * Draw a triangle and fill it
  */
-void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color){
-    line(x0, y0, x1, y1, image, color);
-    line(x0, y0, x2, y2, image, color);
-    line(x1, y1, x2, y2, image, color);
+void triangle(Vec2i A, Vec2i B, Vec2i C, TGAImage &image, TGAColor color){
+        //Sorting points in ascending row order
+        if(A.y > B.y){
+            std::swap(A, B);
+        }
+        if(B.y > C.y){
+            std::swap(B, C);
+        }
+        if(A.y > B.y){
+            std::swap(A, B);
+        }
+
+        //Calculation of vectors
+        Vec2i AB = {B.x - A.x, B.y - A.y};
+        Vec2i AC = {C.x - A.x, C.y - A.y};
+        Vec2i BC = {C.x - B.x, C.y - B.y};
+
+        //Construction of the 2 extreme points for each line
+        Vec2i pAB, pAC;
+
+        //Travel of lines A to B only if A and B on different lines
+        if(AB.y > 0){
+            for(int l = 0 ; l <= AB.y - 1 ; l++){
+                //Segments
+                pAB.x = A.x + AB.x * l / AB.y;
+                pAB.y = A.y + l;
+                pAC.x = A.x + AC.x * l / AC.y;
+                pAC.y = pAB.y;
+
+
+                line(pAB, pAC, image, color);
+            }
+        }else{
+            line(A, B, image, color);
+        }
+
+        Vec2i pBC;
+
+        //Travel of lines B to C only if B and C on different lines
+        if(BC.y > 0){
+            for(int l = AB.y ; l <= AC.y ; l++){
+                pBC.x = B.x + BC.x * (l - AB.y) / BC.y;
+                pBC.y = A.y + l;
+                pAC.x = A.x + AC.x * l / AC.y;
+                pAC.y = pBC.y;
+                line(pBC, pAC, image, color);
+            }
+        }else{
+            line(B, C, image, color);
+        }
 }
 
 int main() {
@@ -76,7 +120,7 @@ int main() {
         int x2 = (point3.x+1.)*width/2.;
         int y2 = (point3.y+1.)*height/2.;
 
-        triangle(x0, y0, x1, y1, x2, y2, image, white);
+        triangle(Vec2i(x0, y0), Vec2i(x1, y1), Vec2i(x2, y2), image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
 
     image.write_tga_file("output.tga");
